136d3c50cd0a588c9c2fdc1f54c5dc0dc8066a18
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-05-20 11:38:41 +0700

    jalankan composition agregasi

--- CompositionAgregasi/CompositionAgregasi.cpp
+++ CompositionAgregasi/CompositionAgregasi.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
-#include <string>
+using namespace std;
+
+#include "HeaderAnak.h"
+#include "HeaderIbu.h"
+
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1= new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAna3 = new anak("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varibu2->tambahAnak(varAnak3);
+	varibu2->tambahAnak(varAnak1);
+
+
 
